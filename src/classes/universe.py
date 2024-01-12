66c808379ca7e8d26f6ea10488dbfe5fa6e52fbe
seda2102 <sedadenboer@outlook.com>
2024-01-12 17:28:14 +0100

    made initialisation function more efficient

--- src/classes/universe.py
+++ src/classes/universe.py
@@ -29,85 +29,60 @@ class Universe(object):
         self.four_vertices_bag = Bag(pool_capacity=Vertex.capacity)
 
         self.size = total_time * initial_slice_size
-        self.triangulation = self.initialize_triangulation()
+        self.triangulation = self.initialise_triangulation()
 
-    def initialize_triangulation(self) -> list["Vertex"]:
+    def initialise_triangulation(self) -> list["Vertex"]:
         """
-        Initialize the grid with vertices and triangles.
+        initialise the grid with vertices and triangles.
 
         Returns:
-            List[[Vertex]]: List of time slices with vertices.
+            List["Vertex"]: List of time slices with vertices.
         """
         time = self.total_time
         width = self.initial_slice_size
 
-        # ---- Initialize vertices and their connections ----
-        # Create empty list to store time slices with spatial nodes
-        vertex_grid = []
-
-        # Loop over all time slices
-        for t in range(time):
-            # Create empty list to store vertices
-            vertices = []
-
-            # Loop over all spatial points
-            for _ in range(width):
-                # Occupy a space in the vertex pool and get the id
-                id = self.vertex_pool.occupy()
-
-                # Add the id to the bag with all candidates
-                self.vertex_bag.add(id)
-
-                # Create vertex
-                vertex = Vertex(id, t)
-
-                # Add vertex to list of vertices of current time slice
-                vertices.append(vertex)
-
-            # Add list of vertices to list of time slices
-            vertex_grid.append(vertices)
+        # ---- Initialise triangles ----
+        # Create grid of blanco vertices
+        vertex_grid = [
+            [Vertex(self.vertex_pool.occupy(), t) for _ in range(width)] for t in range(time)
+        ]
 
         # Create spatial edges
         for t in range(time):
             for i in range(width - 1):
-                # Get vertex
-                vertex = vertex_grid[t][i]
-                vertex.right_neighbour = vertex_grid[t][i + 1]
-            
+                # Set right neighbour
+                vertex_grid[t][i].right_neighbour = vertex_grid[t][i + 1]
+
+            # Set right neighbour for last vertex
             vertex_grid[t][-1].right_neighbour = vertex_grid[t][0]
 
         # Create time-like edges
         for t in range(time - 1):
             for i in range(width - 1):
-                # Get vertices
+                # Set future vertices
                 current_vertex = vertex_grid[t][i]
                 future_vertex1 = vertex_grid[t + 1][i]
                 future_vertex2 = vertex_grid[t + 1][i + 1]
-
-                # Add future neighbour
-                current_vertex.future_vertices.append(future_vertex1)
-                current_vertex.future_vertices.append(future_vertex2)
-
-                # Add past neighbour
+                
+                # Add future vertices to current vertex
+                current_vertex.future_vertices.extend([future_vertex1, future_vertex2])
                 future_vertex1.past_vertices.append(current_vertex)
                 future_vertex2.past_vertices.append(current_vertex)
 
-            # Handle the last vertex differently for periodic boundary conditions
+            # Set future vertices for last vertex
             current_vertex = vertex_grid[t][width - 1]
             future_vertex1 = vertex_grid[t + 1][width - 1]
             future_vertex2 = vertex_grid[t + 1][0]
-
-            # Add future neighbour
-            current_vertex.future_vertices.append(future_vertex1)
-            current_vertex.future_vertices.append(future_vertex2)
-
-            # Add past neighbour
+            
+            # Add future vertices to current vertex
+            current_vertex.future_vertices.extend([future_vertex1, future_vertex2])
             future_vertex1.past_vertices.append(current_vertex)
             future_vertex2.past_vertices.append(current_vertex)
 
-        # # print vertex_grid by id
-        # for time_slice in vertex_grid:
-        #     print([vertex.ID for vertex in time_slice])
+        # ---- CHECKING ----
+        # print vertex_grid by id
+        for time_slice in vertex_grid:
+            print([vertex.ID for vertex in time_slice])
 
         # Print all past and future neighbours
         print("id, right_neighbour, future_vertices, past_vertices")
@@ -115,21 +90,21 @@ class Universe(object):
             for v in t:
                 print(v.ID, v.right_neighbour.ID, [vertex.ID for vertex in v.future_vertices], [vertex.ID for vertex in v.past_vertices])
 
-        # print(self.vertex_pool.elements)
-        # print(self.all_candidates.elements)
-        # print(self.all_candidates.indices)
-        # self.all_candidates.log()
+        print(self.vertex_pool.elements)
+        print(self.vertex_bag.elements)
+        print(self.vertex_bag.indices)
+        self.vertex_bag.log()
 
-        # ---- Initialize triangles ----
+        # ---- Initialise triangles ----
         
-        # Create empty list to store triangles
+        # Create empty list to store triangle layers
         triangle_layers = []
 
-        # Loop over all time slices
+        # Create triangles for each time slice
         for t in range(time - 1):
+            # Create empty list to store triangles
             triangles = []
-            # Loop over all spatial points
-            for i in range(width - 1):
+            for i in range(width):
                 # Occupy two spaces in the triangle pool and get the ids
                 id1 = self.triangle_pool.occupy()
                 id2 = self.triangle_pool.occupy()
@@ -144,16 +119,16 @@ class Universe(object):
 
                 # Set vertices for triangles
                 triangle1.set_vertices(
-                    vl=vertex_grid[t + 1][i],
-                    vr=vertex_grid[t + 1][i + 1],
+                    vl=vertex_grid[(t + 1) % time][i],
+                    vr=vertex_grid[(t + 1) % time][(i + 1) % width],
                     vc=vertex_grid[t][i]
-                    )
-                
+                )
+
                 triangle2.set_vertices(
                     vl=vertex_grid[t][i],
-                    vr=vertex_grid[t][i + 1],
-                    vc=vertex_grid[t + 1][i + 1]
-                    )
+                    vr=vertex_grid[t][(i + 1) % width],
+                    vc=vertex_grid[(t + 1) % time][(i + 1) % width]
+                )
 
                 # Add the triangles to the list of triangles of the vertices
                 triangle1.vl.add_triangle(triangle1)
@@ -168,88 +143,57 @@ class Universe(object):
                 triangles.append(triangle1)
                 triangles.append(triangle2)
 
-            # Handle the last vertex differently for periodic boundary conditions
-            # Occupy two spaces in the triangle pool and get the ids
-            id1 = self.triangle_pool.occupy()
-            id2 = self.triangle_pool.occupy()
-
-            # Add the ids to the bag with all candidates
-            self.triangle_bag.add(id1)
-            self.triangle_bag.add(id2)
-
-            # Create triangles
-            triangle1 = Triangle(id1)
-            triangle2 = Triangle(id2)
-
-            # Set vertices for triangles
-            triangle1.set_vertices(
-                vl=vertex_grid[t + 1][width - 1],
-                vr=vertex_grid[t + 1][0],
-                vc=vertex_grid[t][width - 1]
-                )
-            
-            triangle2.set_vertices(
-                vl=vertex_grid[t][width - 1],
-                vr=vertex_grid[t][0],
-                vc=vertex_grid[t + 1][0]
-                )
-
-            # Add triangles to list of triangles
-            triangles.append(triangle1)
-            triangles.append(triangle2)
-
             # Add list of triangles to list of time slices
             triangle_layers.append(triangles)
 
         # Set the right, left and center triangles for each triangle
         for i, layer in enumerate(triangle_layers):
+            layer_length = len(layer)
             for j, triangle in enumerate(layer):
+                # Get the triangles to the left and right of the current triangle
+                triangle_left = layer[(j - 1) % layer_length]
+                triangle_right = layer[(j + 1) % layer_length]
 
-                # if not on the right edge
-                if j != len(layer) - 1:
-                    triangle.set_triangle_left(layer[j - 1])
-                    triangle.set_triangle_right(layer[j + 1])
-                else:
-                    triangle.set_triangle_left(layer[j - 1])
-                    triangle.set_triangle_right(layer[0])
-
+                # If the triangle is on the first or last layer, the center triangle is None
                 if i == 0:
-                    if triangle.is_downwards():
-                        triangle.set_triangle_center(triangle_layers[i + 1][j + 1])
-                elif i == len(triangle_layers) - 1:
-                    if triangle.is_upwards():
-                        triangle.set_triangle_center(triangle_layers[i - 1][j - 1])
+                    triangle_center = triangle_layers[(i + 1) % time][(j + 1) % layer_length] if triangle.is_downwards() else None
+                # If the triangle is on the second or second to last layer, the center triangle is None
+                elif i == time - 2:
+                    triangle_center = triangle_layers[(i - 1) % time][(j - 1) % layer_length] if triangle.is_upwards() else None
+                # If the triangle is on any other layer, the center triangle is the triangle in the next layer
                 else:
-                    if triangle.is_downwards():
-                        triangle.set_triangle_center(triangle_layers[i + 1][j + 1])
-                    else:
-                        triangle.set_triangle_center(triangle_layers[i - 1][j - 1])
+                    triangle_center = triangle_layers[(i + 1) % time][(j + 1) % layer_length] if triangle.is_downwards() else triangle_layers[(i - 1) % time][(j - 1) % layer_length]
 
-        # # print triangles list with Id
-        # print("\nTriangles:")
-        # for layer in triangle_layers:
-        #     print([triangle.ID for triangle in layer])
+                triangle.set_triangle_left(triangle_left)
+                triangle.set_triangle_right(triangle_right)
+                triangle.set_triangle_center(triangle_center)
+
+        # ---- CHECKING ----
+        # Print triangles list with Id
+        print("\nTriangles:")
+        for layer in triangle_layers:
+            print([triangle.ID for triangle in layer])
         
-        # for layer in triangle_layers:
-        #     print([triangle.type for triangle in layer])
+        for layer in triangle_layers:
+            print([triangle.type for triangle in layer])
 
-        # # Print all triangles and their neighbour triangles
-        # # if none then print none
-        # print("\nid, tl, tr, tc")
-        # for t in triangle_layers:
-        #     for triangle in t:
-        #         print(triangle.ID, triangle.tl.ID if triangle.tl else None, triangle.tr.ID if triangle.tr else None, triangle.tc.ID if triangle.tc else None)
+        # Print all triangles and their neighbour triangles
+        # if none then print none
+        print("\nid, tl, tr, tc")
+        for t in triangle_layers:
+            for triangle in t:
+                print(triangle.ID,";", triangle.tl.ID if triangle.tl else None, triangle.tr.ID if triangle.tr else None, triangle.tc.ID if triangle.tc else None)
 
-        # # Print all triangles
-        # print("\nid, vl, vr, vc")
-        # for t in triangle_layers:
-        #     for triangle in t:
-        #         print(triangle.ID, triangle.vl.ID, triangle.vr.ID, triangle.vc.ID)
+        # Print all triangles
+        print("\nid, vl, vr, vc")
+        for t in triangle_layers:
+            for triangle in t:
+                print(triangle.ID, ";", triangle.vl.ID, triangle.vr.ID, triangle.vc.ID)
 
-        # print()
-        # print(self.triangle_pool.elements)
-        # print(self.triangle_bag.elements)
-        # print(self.triangle_bag.indices)
+        print()
+        print(self.triangle_pool.elements)
+        print(self.triangle_bag.elements)
+        print(self.triangle_bag.indices)
 
     def delete_check(self, vertex):
         """
@@ -278,14 +222,5 @@ class Universe(object):
         return self.size
     
 # Example usage:
-universe = Universe(total_time=3, initial_slice_size=3)
-
-# pool = Pool(capacity=5, silent=True)
-# obj1 = pool.occupy()
-# obj2 = pool.occupy()
-# obj3 = pool.occupy()
-
-# pool.free(obj1)
-
-# obj4 = pool.occupy()
+universe = Universe(total_time=5, initial_slice_size=3)
 
